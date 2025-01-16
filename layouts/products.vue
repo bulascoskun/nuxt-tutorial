e4c55d2601b6de2ee34261866d777f63b771e0c5
Ulaş Coşkun <ulas_coskun99@hotmail.com>
2025-01-16 10:27:00 +0300

    init fetch data

--- layouts/products.vue
+++ layouts/products.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <header>
-      <nav>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
         <NuxtLink to="/products"> Nuxt Dojo Merch </NuxtLink>
       </nav>
     </header>
 
     <!-- page -->
-    <div>
+    <div class="container mx-auto p-4">
       <slot />
     </div>
 
-    <footer>
-      <ul>
+    <footer class="container mx-auto p-4 flex justify-between border-t-2">
+      <ul class="flex gap-4">
         <li>
           <NuxtLink to="/"> Home </NuxtLink>
         </li>
